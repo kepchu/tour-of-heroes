397caca8aad5393c6471456d785ad6b77399d204
Maciej Gut <maciej.gut@ics.ie>
2019-05-13 15:05:43 +0100

    List of selectable heroes in heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -13,11 +14,18 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
+  selectedHero: Hero;
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
+
+  heroes = HEROES;
+
   constructor() { }
 
   ngOnInit() {
+    // this.selectedHero = this.heroes[0];
   }
-
-  
 
 }
